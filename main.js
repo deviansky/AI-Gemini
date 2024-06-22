d6f7762e8696c0cfc7744d501678e6db130f6382
deviansky345 <deviansky345@gmail.com>
2024-06-22 20:12:50 +0000

    Chat_AI_fix

--- main.js
+++ main.js
@@ -14,41 +14,61 @@ let form = document.querySelector('form');
 let promptInput = document.querySelector('input[name="prompt"]');
 let output = document.querySelector('.output');
 
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-pro", // or gemini-1.5-pro
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+  ],
+});
+
+const chat = model.startChat({
+  history: [],
+  generationConfig: {
+    maxOutputTokens: 100
+  }
+});
+
 form.onsubmit = async (ev) => {
   ev.preventDefault();
   output.textContent = 'Generating...';
 
   try {
     // Load the image as a base64 string
-    let imageUrl = form.elements.namedItem('chosen-image').value;
-    let imageBase64 = await fetch(imageUrl)
-      .then(r => r.arrayBuffer())
-      .then(a => Base64.fromByteArray(new Uint8Array(a)));
+    // let imageUrl = form.elements.namedItem('chosen-image').value;
+    // let imageBase64 = await fetch(imageUrl)
+    //   .then(r => r.arrayBuffer())
+    //   .then(a => Base64.fromByteArray(new Uint8Array(a)));
 
     // Assemble the prompt by combining the text with the chosen image
-    let contents = [
-      {
-        role: 'user',
-        parts: [
-          { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
-          { text: promptInput.value }
-        ]
-      }
-    ];
+    // let contents = [
+    //   {
+    //     role: 'user',
+    //     parts: [
+    //       // { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
+    //       { text: promptInput.value }
+    //     ]
+    //   }
+    // ];
 
     // Call the multimodal model, and get a stream of results
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash", // or gemini-1.5-pro
-      safetySettings: [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-      ],
-    });
+    // const genAI = new GoogleGenerativeAI(API_KEY);
+    // const model = genAI.getGenerativeModel({
+    //   model: "gemini-pro", // or gemini-1.5-pro
+    //   safetySettings: [
+    //     {
+    //       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    //       threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    //     },
+    //   ],
+    // });
 
-    const result = await model.generateContentStream({ contents });
+    const prompt = promptInput.value;
+
+    const result = await chat.sendMessageStream(prompt);
 
     // Read from the stream and interpret the output as markdown
     let buffer = [];
